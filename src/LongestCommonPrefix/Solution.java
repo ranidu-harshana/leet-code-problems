9445cfa4737388823972e6d51523c69a31eb8e94
raniduharshana <rharshana@lk.onsystechnologies.com>
2025-04-11 02:07:50 +0530

    https://leetcode.com/problems/longest-common-prefix

---
+++ src/LongestCommonPrefix/Solution.java
@@ -0,0 +1,30 @@
+package LongestCommonPrefix;
+
+/*
+* https://leetcode.com/problems/longest-common-prefix
+ */
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 1) {
+            return strs[0];
+        }
+
+        StringBuilder finalString = new StringBuilder();
+
+        for(int i = 0; i < strs[0].length(); i++) {
+            for(int j = 1; j < strs.length; j++) {
+                if(i > strs[j].length() - 1) {
+                    return finalString.toString();
+                }
+
+                if(strs[j].isEmpty() | strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return finalString.toString();
+                }
+            }
+
+            finalString.append(strs[0].charAt(i));
+        }
+
+        return finalString.toString();
+    }
+}
